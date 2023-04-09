0b6202222a8dd801734aa2906a4eb2216144a16c
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-04-09 00:40:14 -0400

    refactored to safely overwrite

--- Contents/mods/ProximityInvites/media/lua/client/ProximityInvites - factions.lua
+++ Contents/mods/ProximityInvites/media/lua/client/ProximityInvites - factions.lua
@@ -1,71 +1,15 @@
---media/lua/client/
 require "ISUI/UserPanel/ISFactionAddPlayerUI"
 
 local util = require "ProximityInvites - utility"
 
-function ISFactionAddPlayerUI:onClick(button)
-    if button.internal == "CANCEL" then
-        self:setVisible(false)
-        self:removeFromUIManager()
-    end
-    if button.internal == "ADDPLAYER" then
-        if not self.changeOwnership then
-            if util.validPlayerToPlayerDistance(self.player, getPlayerFromUsername(self.selectedPlayer)) then
-                local modal = ISModalDialog:new(0,0, 350, 150, getText("IGUI_FactionUI_InvitationSent",self.selectedPlayer), false, nil, nil)
-                modal:initialise()
-                modal:addToUIManager()
-                sendFactionInvite(self.faction, self.player, self.selectedPlayer)
-            end
-        else
-            self.faction:setOwner(self.selectedPlayer)
-            self.factionUI.isOwner = false
-            self.factionUI:populateList()
-            self.factionUI:updateButtons()
-            self:setVisible(false)
-            self:removeFromUIManager()
-            self.faction:syncFaction()
-        end
-    end
-end
-
-
---IGUI_outOfRange
+local ISFactionAddPlayerUI_populateList = ISFactionAddPlayerUI.populateList
 function ISFactionAddPlayerUI:populateList()
-    self.playerList:clear()
-    self.addPlayer.enable = false
-    if not self.scoreboard then return end
-    for i=1,self.scoreboard.usernames:size() do
-        local username = self.scoreboard.usernames:get(i-1)
-        local displayName = self.scoreboard.displayNames:get(i-1)
-        local doIt = false
-        if self.changeOwnership then
-            doIt = not self.faction:isOwner(username)
-        else
-            doIt = username ~= self.player:getUsername() and not self.faction:isMember(username)
-        end
-        if doIt then
-            local newPlayer = {}
-            newPlayer.name = username
-            if self.changeOwnership then
-                local alreadyFaction = self.faction:isMember(username)
-                if not alreadyFaction then
-                    newPlayer.tooltip = getText("IGUI_FactionUI_NoMember")
-                end
-            else
-                local alreadyFaction = Faction.isAlreadyInFaction(username)
-                if alreadyFaction then
-                    newPlayer.tooltip = getText("IGUI_FactionUI_AlreadyHaveFaction")
-                end
-            end
-
-            if not util.validPlayerToPlayerDistance(self.player, getPlayerFromUsername(username)) then
-                newPlayer.tooltip = (newPlayer.tooltip and newPlayer.tooltip.." " or "") .. getText("IGUI_outOfRange")
-            end
-
-            local item = self.playerList:addItem(displayName, newPlayer)
-            if newPlayer.tooltip then
-                item.tooltip = newPlayer.tooltip
-            end
+    ISFactionAddPlayerUI_populateList(self)
+    if self.changeOwnership then return end
+    for i=1, self.playerList:size() do
+        local item = self.playerList.items[i]
+        if item and item.username and (not util.validPlayerToPlayerDistance(self.player, getPlayerFromUsername(item.username))) then
+            item.tooltip = (item.tooltip and item.tooltip.." " or "") .. getText("IGUI_outOfRange")
         end
     end
 end
