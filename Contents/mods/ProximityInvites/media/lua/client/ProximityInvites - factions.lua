1585c31ef14a48aa0c2d2cb465902b10a1b4ba15
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-07-13 04:14:04 -0400

    add debug messages

--- Contents/mods/ProximityInvites/media/lua/client/ProximityInvites - factions.lua
+++ Contents/mods/ProximityInvites/media/lua/client/ProximityInvites - factions.lua
@@ -9,6 +9,9 @@ function ISFactionAddPlayerUI:populateList()
     for i=1, self.playerList:size() do
         local item = self.playerList.items[i]
         local playerA, playerB = self.player, getPlayerFromUsername(item.username)
+
+        if not playerB then print("WARNING: proximityInvite can't find: "..item.username) end
+
         if item and item.username and playerA and playerB and (not util.validPlayerToPlayerDistance(playerA, playerB) ) then
             item.tooltip = (item.tooltip and item.tooltip.." " or "") .. getText("IGUI_outOfRange")
         end
