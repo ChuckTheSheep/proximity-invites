1190e72d5bb43680b38e852fb9d593d13fd956c9
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-23 21:19:47 -0500

    Update ProximityInvites - factions.lua

--- Contents/mods/ProximityInvites/media/lua/client/ProximityInvites - factions.lua
+++ Contents/mods/ProximityInvites/media/lua/client/ProximityInvites - factions.lua
@@ -6,13 +6,19 @@ local ISFactionAddPlayerUI_populateList = ISFactionAddPlayerUI.populateList
 function ISFactionAddPlayerUI:populateList()
     ISFactionAddPlayerUI_populateList(self)
     if self.changeOwnership then return end
-    for i=1, self.playerList:size() do
+
+    for i=1,self.scoreboard.usernames:size() do
+
+        local username = self.scoreboard.usernames:get(i-1)
+        local playerA, playerB = self.player, getPlayerFromUsername(username)
         local item = self.playerList.items[i]
-        local playerA, playerB = self.player, getPlayerFromUsername(item.username)
 
         --if not playerB then print("WARNING: proximityInvite can't find: "..item.username) end
-        if not item or not item.username or not playerB or (not util.validPlayerToPlayerDistance(playerA, playerB) ) then
-            item.tooltip = (item.tooltip and item.tooltip.." " or "") .. getText("IGUI_outOfRange")
+        if not username or not playerB or not util.validPlayerToPlayerDistance(playerA, playerB) then
+            if item then
+                self.playerList.items[i].tooltip = (item.tooltip and item.tooltip.." " or "") .. getText("IGUI_outOfRange")
+            end
         end
     end
+
 end
